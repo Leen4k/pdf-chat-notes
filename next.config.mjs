5256d639be3668d7f3732a307e0e9fb9ed45e711
wathnak <110277798+Leen4k@users.noreply.github.com>
2024-12-30 16:50:34 +0700

    add redis cache

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+  },
 };
 
 export default nextConfig;
