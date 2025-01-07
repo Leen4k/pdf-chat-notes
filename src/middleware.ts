9be9622b4bf2888b23f5d163c5da56a17b50966f
wathnak <110277798+Leen4k@users.noreply.github.com>
2025-01-07 17:12:06 +0700

    fix text editor

--- src/middleware.ts
+++ src/middleware.ts
@@ -75,8 +75,5 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/(api|trpc)(.*)"],
 };
