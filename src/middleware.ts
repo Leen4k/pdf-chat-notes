ab6557ebbdccf5023ff2c1aa9118298f7130f086
wathnak <110277798+Leen4k@users.noreply.github.com>
2025-01-17 23:16:09 +0700

    resize og image

--- src/middleware.ts
+++ src/middleware.ts
@@ -75,5 +75,8 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|gif|svg)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
